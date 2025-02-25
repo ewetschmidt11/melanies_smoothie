ae32c08761919c2cdd494eb2a39b85fcb6bc1cde
ewetschmidt11 <schmidt.tobias@ewe-netz.de>
2025-02-25 16:30:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.write("The name on the Smoothie will be:", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
+st.stop()
 
 # Convert
 pd_df = my_dataframe.to_pandas()
